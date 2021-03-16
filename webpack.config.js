2afdbdd980a30f43d10b046e327be52825cd42cc
Fernando Resendiz <resendizbautista801@gmail.com>
2021-03-15 19:35:36 -0600

    Added webpack aliases

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,12 @@ module.exports = {
   },
   resolve: {
     extensions: [".js"],
+    alias: {
+      '@utils': path.resolve(__dirname, "src/utils/"),
+      '@templates': path.resolve(__dirname, "src/templates/"),
+      '@styles': path.resolve(__dirname, "src/styles/"),
+      '@images': path.resolve(__dirname, "src/assets/images/"),
+    }
   },
   module: {
     rules: [
@@ -43,7 +49,7 @@ module.exports = {
             mimetype: "aplication/font-woff",
             name: "[name].[contenthash].[ext]",
             outputPath: "./assets/fonts",
-            publicPath: "./assets/fonts",
+            publicPath: "../assets/fonts",
             esModule: false,
           },
         },
