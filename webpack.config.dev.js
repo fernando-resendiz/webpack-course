706ecebcfd082dce73b924b23bdc5d71c989d0c0
Fernando Resendiz <resendizbautista801@gmail.com>
2021-03-31 17:36:51 -0600

    Added devtool source-map

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -15,6 +15,7 @@ module.exports = {
     assetModuleFilename: "assets/images/[hash][ext][query]",
   },
   mode: 'development',
+  devtool: 'source-map',
   resolve: {
     extensions: [".js"],
     alias: {
